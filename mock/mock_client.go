6bcaa08ad25da60d05eae4087912621e4f188e46
Wout Slakhorst <wout.slakhorst@nedap.com>
2019-10-16 16:58:29 +0200

    update mock
    Signed-off-by: Wout Slakhorst <wout.slakhorst@nedap.com>

--- mock/mock_client.go
+++ mock/mock_client.go
@@ -105,3 +105,18 @@ func (mr *MockRegistryClientMockRecorder) RegisterOrganization(arg0 interface{})
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RegisterOrganization", reflect.TypeOf((*MockRegistryClient)(nil).RegisterOrganization), arg0)
 }
+
+// ReverseLookup mocks base method
+func (m *MockRegistryClient) ReverseLookup(name string) (*db.Organization, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "ReverseLookup", name)
+	ret0, _ := ret[0].(*db.Organization)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// ReverseLookup indicates an expected call of ReverseLookup
+func (mr *MockRegistryClientMockRecorder) ReverseLookup(name interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ReverseLookup", reflect.TypeOf((*MockRegistryClient)(nil).ReverseLookup), name)
+}
